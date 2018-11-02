6b3d9679659ef742f044033d8677dbc324c21193
AlbulescuDaniel <albulescu.daniel1@gmail.com>
2018-11-02 17:36:01 +0200

    chqnged name

--- src/main/java/net/licenta/LicentaApplication.java
+++ src/main/java/net/licenta/LicentaApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LicetaApplication {
+public class LicentaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LicetaApplication.class, args);
+		SpringApplication.run(LicentaApplication.class, args);
 	}
 }
