44759a2fbff772ce6a220570fb8e5d51700369b3
AlbulescuDaniel <albulescu.daniel1@gmail.com>
2018-11-02 17:22:21 +0200

    First commit

---
+++ src/main/java/net/licenta/LicetaApplication.java
@@ -0,0 +1,12 @@
+package net.licenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicetaApplication.class, args);
+	}
+}
