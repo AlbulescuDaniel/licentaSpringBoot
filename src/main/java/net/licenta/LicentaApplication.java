5e4e53c7d694eaa4a35c506f190e17ae5897355e
AlbulescuDaniel <albulescu.daniel1@gmail.com>
2018-11-02 18:55:00 +0200

    added default security and connection with remote postgre database

--- src/main/java/net/licenta/LicentaApplication.java
+++ src/main/java/net/licenta/LicentaApplication.java
@@ -2,11 +2,21 @@ package net.licenta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
+@RequestMapping(value = "/test")
 public class LicentaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LicentaApplication.class, args);
+	}
+	
+	@GetMapping
+	public String testApi() {
+		return "Is on";
 	}
 }
