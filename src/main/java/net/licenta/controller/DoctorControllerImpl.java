344aaecc51388a2768c4aba52b24c40bbb36f3df
daniel.albulescu <daniel.albulescu@BUK30LAP4000936.client.ro.r4.madm.net>
2018-11-13 09:00:34 +0200

    test

--- src/main/java/net/licenta/controller/DoctorControllerImpl.java
+++ src/main/java/net/licenta/controller/DoctorControllerImpl.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.annotation.Timed;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
